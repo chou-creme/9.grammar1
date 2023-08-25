c01e75f8021bffae2e9487fa6eb77db769d374ae
sakura <chou.creme.294@gmail.com>
2023-08-25 21:48:06 +0900

    文法基本1課題提出

---
+++ ArgsLastPrinter.java
@@ -0,0 +1,9 @@
+public class ArdsLastPrinter {
+	public static void main(String[] args) {
+		if (args.length == 0) {
+			System.out.println("なし");
+		} else {
+			System.out.println("[args.n-1]");
+		}
+	}
+}
